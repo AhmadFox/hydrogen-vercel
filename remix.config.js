4e40b90a9445ea13333abd59e936d58c58b6bef8
Ahmad Issa Gharaibeh <GHARAIBEH.AHMAD88@GMAIL.COM>
2025-06-18 21:30:09 +0300

    Initial commit for Hydrogen Vercel deployment

---
+++ remix.config.js
@@ -0,0 +1,10 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+module.exports = {
+	serverBuildTarget: "vercel",
+	server: "./vercel-server.js",
+	ignoredRouteFiles: ["**/.*"],
+	future: {
+	  v3_routeConvention: true,
+	},
+  };
+  
